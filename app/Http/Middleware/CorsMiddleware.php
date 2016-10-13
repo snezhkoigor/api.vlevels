3479ab9abe0e4be07a8580e2f11baf62d09d1157
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-10-13 19:05:38 +0300

    CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -22,9 +22,10 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
         return $next($request)
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
     }
 }
