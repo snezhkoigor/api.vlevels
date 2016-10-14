9772753e861c7a6de88007a85be0c5ef80c8cb65
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-10-14 10:56:37 +0300

    CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -21,9 +21,15 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+        header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Allow-Origin: *");
-//        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-//        header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type, X-Auth-Token, Origin, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header( "HTTP/1.1 200 OK" );
+            exit();
+        }
 
         return $next($request);
 //            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
