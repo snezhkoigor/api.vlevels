a124fd096e49f3f7cf2a0c73ac2c53fdf0f279e4
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-10-14 11:14:34 +0300

    CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,6 +9,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class CorsMiddleware
 {
@@ -26,11 +27,23 @@ class CorsMiddleware
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-            header( "HTTP/1.1 200 OK" );
+        $response = $next($request)
+            ->header('Access-Control-Allow-Origin: *')
+            ->header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization')
+            ->header('Access-Control-Allow-Credentials: true');
+
+        if ($request->getMethod() == 'OPTIONS' && $response->getStatusCode() == 405) {
+            return new Response('', 204, $response->headers->all());
         }
 
-        return $next($request);
+        return $response;
+//
+//        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//            header( "HTTP/1.1 200 OK" );
+//        }
+//
+//        return $next($request);
 //            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 //            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
     }
