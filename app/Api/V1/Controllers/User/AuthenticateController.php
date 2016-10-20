fc23b145d6add67ddf427c475a3a93ab32f605e8
i.s.sergeevich@yandex.ru <snezhkoigor>
2016-10-20 11:36:33 +0300

    add activation

--- app/Api/V1/Controllers/User/AuthenticateController.php
+++ app/Api/V1/Controllers/User/AuthenticateController.php
@@ -43,7 +43,7 @@ class AuthenticateController extends Controller
         try {
             // attempt to verify the credentials and create a token for the user
             if (!$token = JWTAuth::attempt($credentials)) {
-                $this->response->errorUnauthorized(json_encode(['System' => 'Invalid credentials.']));
+                $this->response->errorUnauthorized(json_encode(['System' => ['Invalid credentials.']]));
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
