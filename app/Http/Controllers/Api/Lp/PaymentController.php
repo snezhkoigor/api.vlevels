1216cd524eb9c02283927f2515326170439f3b41
igorsnezko <i.s.sergeevich@yandex.ru>
2017-02-08 21:08:08 +0300

    add visa and mc

--- app/Http/Controllers/Api/Lp/PaymentController.php
+++ app/Http/Controllers/Api/Lp/PaymentController.php
@@ -112,6 +112,35 @@ class PaymentController extends Controller
                     $gateway->setLabel($invoice->_comment);
                     $gateway->setPassword('CH+/mBSKzhlKvoX8uKG56att');
                     $gateway->setOrderId($request->invoiceId);
+                    $gateway->setMethod('PC');
+                    $gateway->setReturnUrl('http://vlevels.ru/success');
+                    $gateway->setCancelUrl('http://vlevels.ru/fail');
+
+                    $response = $gateway->purchase(['amount' => $invoice->_amount, 'currency' => 'RUB', 'testMode' => false, 'FormComment' => $request->formComment])->send();
+
+                    $result = [
+                        'actionUrl' => $response->getEndpoint(),
+                        'method' => $response->getRedirectMethod(),
+                        'params' => $response->getRedirectData()
+                    ];
+
+                    break;
+
+                case 'MC':
+                case 'VISA':
+                    DB::connection('oldMysql')
+                        ->table('payment')
+                        ->where('_invoce', '=', $request->invoiceId)
+                        ->update([
+                            '_payment_type' => 'Yandex.Money',
+                            '_fee' => round(($invoice->_amount*0.5)/100, 2)
+                        ]);
+
+                    $gateway = Omnipay::create('\yandexmoney\YandexMoney\GatewayIndividual');
+                    $gateway->setAccount('41001310031527');
+                    $gateway->setLabel($invoice->_comment);
+                    $gateway->setPassword('CH+/mBSKzhlKvoX8uKG56att');
+                    $gateway->setOrderId($request->invoiceId);
                     $gateway->setMethod('AC');
                     $gateway->setReturnUrl('http://vlevels.ru/success');
                     $gateway->setCancelUrl('http://vlevels.ru/fail');
